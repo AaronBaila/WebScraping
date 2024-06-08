4494937aa7d0d84c189d35d16f76092c4fbb1a9f
Your Name <you@example.com>
2024-06-08 21:12:52 +0200

    Carrefour 1.1

--- bbdd.js
+++ bbdd.js
@@ -2,15 +2,9 @@
 import { MongoClient } from "mongodb";
 
 // ****** VARIABLES ******
-//Variable que almacena la URL de la BBDD
-//const dbURL = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.0"
-const dbURL = "mongodb://localhost:27017"
+const dbURL = "mongodb://127.0.0.1:27017" //Variable que almacena la URL de la BBDD
 
-//export let db //Variable que va almacenar la conexion con la BBDD
-
-//****** Ejecucion de la funcion principal ******
-//crearBBDD() //Llamo a la funcion que conecta con la BBDD y crea la BBDD
-
+// ****** FUNCIONES ******
 //Funcion que conecta con la BBDD y crea la BBDD
 export async function crearBBDD(){
     let conexion = await MongoClient.connect(dbURL)
@@ -18,66 +12,3 @@ export async function crearBBDD(){
     console.log("Conectado a la BBDD, con URL: " + dbURL)
     return db
 }
-
-
-// ****** FUNCIONES PRUEBA (APRENDIZAJE) ******
-
-/*conexionBBDD()
-
-export async function conexionBBDD(){
-    await crearBBDD()
-}*/
-
-/*export async function insertarDatos(){
-    let datosInsertados = await coleccion.insertOne(producto)
-    console.log("Datos insertados correctamente: " + datosInsertados)
- }
-
-export async function obtenerDatos(){
-    let datos = await coleccion.find().toArray()
-    console.log("DATOS: ")
-    console.log(datos)
-}
-
-export async function deleteBBDD(){
-    let res = await db.dropDatabase()
-    console.log("BBDD Eliminada: " + res)
-}
-
-
-//FUNCIONES CON THEN Y CATCH EN VEZ DE ASYNC AWAIT
-
-export function conexionBBDD(){
-    MongoClient.connect(dbURL)
-    .then(client =>{
-        db = client.db('miDB')
-        console.log("Conectado a la BBDD, con URL: " + dbURL)
-        coleccion = db.collection("miColeccion")
-    })
-    .catch(error =>{console.log(error)})
-}
-
-export function insertarDatos(){
-   coleccion.insertOne(producto)
-    .then(results=>{
-        console.log("Datos almacenados correctamente: " + results)
-    })
-    .catch(error =>{
-        console.log(error)
-    })
-}
-
-export function obtenerDatos(){
-    coleccion.find().toArray()
-    .then(results=>{
-        console.log(results)
-    })
-    .catch(error =>{
-        console.log(error)
-    })
-}
-
-export function deleteBBDD(){
-    db.dropDatabase()
-    console.log("BBDD Eliminada")
-}*/
--- scrapingCarrefour.js
+++ scrapingCarrefour.js
@@ -67,7 +67,6 @@ scraping()
 
 async function scraping(){
 
-
     // PRUEBAS LOGFILE ****************************************************************** *
 
     // Crear el archivo
@@ -89,10 +88,12 @@ async function scraping(){
     await pagina.click('button.collage-menu__trigger.icon-menu-nav')
 
     //Click en Supermercado
-    await pagina.waitForSelector('ul.collage-new-menu__list > li.menu-entry--first-level')
-    await pagina.click('ul.collage-new-menu__list > li.menu-entry--first-level')
+    await pagina.waitForTimeout(2000)
+    //await pagina.waitForSelector('ul > li:nth-child(1)')
+    await pagina.click('ul > li:nth-child(1)')
 
     categoriasPrincipales = await obtenerCategorias()
+    categoriasPrincipales.shift() //Elimino el primer elemento (ofertas)
 
     await almacenarProductos()
 
@@ -106,18 +107,22 @@ async function scraping(){
 
 async function almacenarProductos(){
 
+    console.log("DENTRO DE ALMACENAR PRODUCTOS")
+
     let finalPaginas = true
     let primeraEntrada = true
 
     //Click en la primera categoria principal
-    await pagina.waitForSelector('ul.collage-new-menu__list > li.menu-entry--no-border:nth-child(2)')
-    await pagina.click('ul.collage-new-menu__list > li.menu-entry--no-border:nth-child(2)')
+    await pagina.waitForSelector('ul > li:nth-child(2)')
+    await pagina.click('ul > li:nth-child(2)')
+    //WAIT DE TEST
     
     for(let x=0; x<=categoriasPrincipales.length-1;x++){
         console.log(" ******** Categoria ******** ")
         console.log(categoriasPrincipales[x])
 
         subCategorias = await obtenerSubcategorias()
+        subCategorias.shift()
 
         for(let i = 0; i<=subCategorias.length-1;i++){
             console.log(" ******** SubCategoria ******** ")
@@ -125,8 +130,8 @@ async function almacenarProductos(){
 
             if(primeraEntrada){
                 //Click en Carniceria (primera subcategoria de la categoria principal)
-                await pagina.waitForSelector('ul.collage-new-menu__list li.menu-entry--no-border:nth-child(2) a')
-                await pagina.click('ul.collage-new-menu__list li.menu-entry--no-border:nth-child(2) a')
+                await pagina.waitForSelector('div.collage-new-menu__list-container > ul > li:nth-child(2)')
+                await pagina.click('div.collage-new-menu__list-container > ul > li:nth-child(2)')
                 await pagina.waitForSelector('div.product-card__detail')
                 primeraEntrada = false
             }
@@ -141,12 +146,36 @@ async function almacenarProductos(){
                 // Simula el scroll hacia abajo para cargar los elementos 'lazy'
                 await autoScroll(pagina);
 
-                await pagina.waitForTimeout(500)
+                await pagina.waitForTimeout(1000)
 
                 //Obtengo los div que contienen todos los datos de un producto y que pertenezcan a el contenedor padre div.product-card-list
                 //Si no especifico que obtenga solo los productos del contenedor padre, obtiene tambien los productos de oferta y por lo tanto habrian repetidos en la BBDD
-                let contenedorPadre = await pagina.$('div.product-card-list')
-                let productosDOM = await contenedorPadre.$$('div.product-card__detail')
+                //let contenedorPadre = await pagina.$('div.product-card-list')
+
+                /* PRUEBAS */
+                let contenedorPadre
+                let productosDOM
+                try{
+                    contenedorPadre = await pagina.$('div.product-card-list')
+                    productosDOM = await contenedorPadre.$$('div.product-card__detail')
+                }catch(error){
+                    //await pagina.waitForTimeout(1200000)
+                    await pagina.waitForTimeout(2000)
+                    console.log("ERROR 1:")
+                    console.log(error)
+                    console.log("ERROR POR POPUP")
+                    try{
+                        await pagina.click('.icon-cross-thin')
+                        await pagina.waitForTimeout(2000)
+                    }catch(error){
+                        await pagina.waitForTimeout(2000)
+                        console.log("ERROR 2:")
+                        console.log(error)
+                    }
+                    productosDOM = await contenedorPadre.$$('div.product-card__detail')
+                }
+
+                //let productosDOM = await contenedorPadre.$$('div.product-card__detail')
 
                 // *** OBTIENE PRODUCTOS UNO A UNO ***
                 //Bucle que obtiene uno a uno los elementos indicados del DOM (variable productosDOM)
@@ -156,6 +185,7 @@ async function almacenarProductos(){
                     try{
                         //Sacamos todos los H4 de el div sacado anteriormente y los pasamos a texto
                         let titulo = await pagina.evaluate((el) => el.querySelector("a").textContent, elemento)
+                        let precio = await pagina.evaluate((el) => el.querySelector("span").textContent, elemento)
                         // Escribir en el archivo logfile
                         fs.appendFileSync(rutaArchivo, titulo)
                         fs.appendFileSync(rutaArchivo, '\n')
@@ -164,6 +194,7 @@ async function almacenarProductos(){
                         //Creo un objeto de nombre producto el cual tiene como atributos datos de cada producto escrapeado
                         let producto = {
                             "titulo": titulo.trim(), //Uso .trim() para formatear la string de manera que elimine los espacios a la izquierda y a la derecha pero dejando intacto el contenido del string
+                            "precio": precio.trim(),
                             "tienda": "Carrefour"
                         }
 
@@ -187,17 +218,10 @@ async function almacenarProductos(){
                 }
 
                 numeroWhiles = numeroWhiles + 1
-
-                //Prueba
-                /*
-                if(numeroWhiles === 3){
-                    finalPaginas = false
-                }*/
-
             }
 
             //Pasamos a la siguiente subcategoria
-            if(i+3 !== subCategorias.length-1){
+            if(/*i+3*/ i !== subCategorias.length-1){
                 await pagina.click('div.nav-first-level-categories__slide:nth-child('+(i+3)+') > a') //.nav-first-level-categories__list-element.ripple > span.nav-first-level-categories__text
                 await pagina.waitForTimeout(1000)
             }else{
@@ -205,7 +229,7 @@ async function almacenarProductos(){
             }  
         }
 
-        if(x+2 <= categoriasPrincipales.length-1){
+        if(/*x+2*/ x <= categoriasPrincipales.length-1){
             //Click en Menu
             await pagina.click('button.collage-menu__trigger.icon-menu-nav')
 
@@ -225,12 +249,14 @@ async function almacenarProductos(){
 
 
 async function obtenerCategorias(){
-   // Espera a que el elemento que quieres interactuar esté disponible en la página
-  await pagina.waitForSelector('ul.collage-new-menu__list li.menu-entry--no-border span.menu-entry__item__text')
 
-  // Extrae el contenido de todas las etiquetas <span> dentro de la lista
-  let categoriasPrincipales = await pagina.evaluate(() => {
-    let spanElements = document.querySelectorAll('ul.collage-new-menu__list li.menu-entry--no-border span.menu-entry__item__text')
+    //await pagina.waitForTimeout(2000)
+    // Espera a que el elemento que quieres interactuar esté disponible en la página
+    await pagina.waitForSelector('div.collage-new-menu__list-container > ul > li')
+
+    // Extrae el contenido de todas las etiquetas <span> dentro de la lista
+    let categoriasPrincipales = await pagina.evaluate(() => {
+    let spanElements = document.querySelectorAll('div.collage-new-menu__list-container > ul > li')
     let titulosCategorias = []
     spanElements.forEach(span => {
         titulosCategorias.push(span.textContent.trim());
@@ -244,11 +270,11 @@ async function obtenerCategorias(){
 
 async function obtenerSubcategorias(){
     // Espera a que el elemento que quieres interactuar esté disponible en la página
-   await pagina.waitForSelector('ul.collage-new-menu__list li.menu-entry.menu-entry--no-border div.menu-entry__item a.anchor span.menu-entry__item__text')
+   await pagina.waitForSelector('div.collage-new-menu__list-container > ul > li')
  
    // Extrae el contenido de todas las etiquetas <span> dentro de la lista
    let subCategorias = await pagina.evaluate(() => {
-     let spanElements = document.querySelectorAll('ul.collage-new-menu__list li.menu-entry.menu-entry--no-border div.menu-entry__item a.anchor span.menu-entry__item__text')
+     let spanElements = document.querySelectorAll('div.collage-new-menu__list-container > ul > li')
      let titulosSubcategorias = []
      spanElements.forEach(span => {
          titulosSubcategorias.push(span.textContent.trim());
